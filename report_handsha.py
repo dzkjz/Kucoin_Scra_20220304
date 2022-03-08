d1f61a477e69856478a6c2196844f8df85c89f64
dzkjz <dzkjzdzkjz@gmail.com>
2022-03-09 01:18:24 +0800

    update 20220309 检测移除重复，投完停止

--- report_handsha.py
+++ report_handsha.py
@@ -8,8 +8,9 @@ while True:
     path_url_txt = 'data_saved/url.txt'
     link_paras = reader.txt_reader(path_url_txt)
     link_paras = list(dict.fromkeys(link_paras))  # 去重
-    if link_paras is not None:
+    if link_paras is not None and len(link_paras) > 0:
         if len(link_paras) > 0:
+            print("有链接")
             link_para = link_paras[0]
             print(f'投诉 {link_para} 开始！')
             try:
@@ -19,11 +20,14 @@ while True:
                     lines.remove(f"{link_para}\n")
                     with open(path_url_txt, "w") as new_f:
                         for line in lines:
+                            print(f'{line} 检测移除')
                             new_f.write(line)
 
                 print(f'投诉 {link_para} 完毕！')
             except:
                 print(f'投诉 {link_para} 遇到问题 暂时不管！')
+                continue
 
     else:
+        print("投完了")
         break
